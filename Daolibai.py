cd97de9974417183d80d9c2e8fed695e5df15e65
bnyu <xuyue06@outlook.com>
2017-04-16 10:44:53 +0800

    Update Daolibai.py

--- Daolibai.py
+++ Daolibai.py
@@ -65,8 +65,8 @@ def animated(y):
         line_pendulum.set_data([(y[ii, 2], y[ii, 2] + pendulum_x),
                                 (wheel_radius + cart_high, wheel_radius + cart_high + pendulum_y)])
         return (line_wheel_left, line_wheel_right, line_cart_bottom, line_cart_top,
-                line_wheel_left, line_cart_right, line_hinge, line_pendulum)
+                line_cart_left, line_cart_right, line_hinge, line_pendulum)
 
-    ani = animation.FuncAnimation(fig, animate, frames=1000, interval=20, init_func=None)
+    ani = animation.FuncAnimation(fig, animate, frames=1000, interval=20, blit=True, init_func=None)
     plt.show(ani)
 
