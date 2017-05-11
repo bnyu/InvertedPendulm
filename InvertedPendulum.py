1d92971d2b40dc7e2f5c70123eb1704b9794277b
bnyu <xuyue06@outlook.com>
2017-05-12 02:05:53 +0800

    version 1.0

--- InvertedPendulum.py
+++ InvertedPendulum.py
@@ -8,15 +8,13 @@ import matplotlib.animation as animation
 matplotlib.use('TkAgg')
 
 
-# 作轨迹图
-def track(y, t, rd, step):
-    if t==2000:
-        t-=1
-    time = np.arange(0, t+1)
-    s = np.zeros(t+1)
-    if step!=0:
-        for j in range(step, t+1):
-            s[j]=4
+# 开环响应 闭环响应 轨迹图
+def track(y, t, rd, step1, step0, size=2.0):
+    time = np.arange(0, t)
+    s = np.zeros(t)
+    if step1!=0:
+        for j in range(step1, step0):
+            s[j]=size
     angle = y[:, 0]
     angle_velocity = y[:, 1]
     position = y[:, 2]
@@ -43,7 +41,7 @@ def track(y, t, rd, step):
     plt.show(fig)
 
 
-# 作动画
+# 动画仿真
 def animated(y, t):
     cart_high = 3
     cart_width = 5
@@ -54,7 +52,7 @@ def animated(y, t):
     wheel = [wheel_radius*np.sin(circle), wheel_radius*np.cos(circle)]
     hinge = [hinge_radius*np.sin(circle), hinge_radius*np.cos(circle)]
     fig = plt.figure()
-    ax = plt.axes(xlim=(-5, 5), ylim=(0, 10))
+    ax = plt.axes(xlim=(-15, 15), ylim=(0, 20))
     line_wheel_left, = ax.plot([], [], lw=2, color='g')
     line_wheel_right, = ax.plot([], [], lw=2, color='g')
     line_cart_left, = ax.plot([], [], lw=2, color='k')
@@ -79,7 +77,8 @@ def animated(y, t):
         pendulum_x = length*math.sin(y[ii, 0])
         pendulum_y = length*math.cos(y[ii, 0])
         line_pendulum.set_data([(y[ii, 2], y[ii, 2] + pendulum_x),
-                                (wheel_radius + cart_high, wheel_radius + cart_high + pendulum_y)])
+                                (wheel_radius + cart_high,
+                                 wheel_radius + cart_high + pendulum_y)])
         return (line_wheel_left, line_wheel_right, line_cart_bottom, line_cart_top,
                 line_cart_left, line_cart_right, line_hinge, line_pendulum)
 
@@ -94,9 +93,12 @@ def model():
         pendulum_mass = 0.1
         pendulum_length = 2.0
     else:
-        cart_mass = 0.5*min(3, max(1, int(input('请选择小车质量：1:(0.5kg)，2:(1.0kg)，3:(2.0kg)'))))
-        pendulum_mass = 0.05*min(3, max(1, int(input('请选择摆杆质量：1:(0.05kg)，2:(0.1kg)，3:(0.2kg)'))))
-        pendulum_length = 1.0*min(3, max(1, int(input('请选择摆杆长度：1:(1.0m)，2:(2.0m)，3:(4.0m)'))))
+        cart_mass = (0.5*min(3, max(1, int
+        (input('请选择小车质量：1:(0.5kg)，2:(1.0kg)，3:(1.5kg)，4:(2.0kg)')))))
+        pendulum_mass = (0.05*min(3, max(1, int
+        (input('请选择摆杆质量：1:(0.05kg)，2:(0.1kg)，3:(0.15kg)，4:(0.2kg)')))))
+        pendulum_length = (1.0*min(3, max(1, int
+        (input('请选择摆杆长度：1:(1.0m)，2:(2.0m)，3:(3m)，4:(4.0m)')))))
     gravity = 9.81
     a21 = (cart_mass + pendulum_mass) * gravity / (cart_mass * pendulum_length / 2)
     a41 = - pendulum_mass * gravity / cart_mass
@@ -108,6 +110,10 @@ def model():
 
 
 def calculate(a, b):
+    print('A=' ,a)
+    print('B=', b)
+    c = np.eye(4)
+    print('C=', c)
     print('\n正在计算倒立摆系统能控性...')
     temp = b
     h = temp.T
@@ -116,10 +122,45 @@ def calculate(a, b):
         h = np.vstack((h, temp.T))
     d = np.linalg.det(h)
     if d != 0:
+        print('We=', h, '为行满秩')
         print("系统是完全能控的。")
     else:
+        print('We=', h, '不为行满秩')
         print("系统是不完全能控的。")
         exit(1)
+
+    print('\n正在计算倒立摆系统能观性...')
+    temp2 = c
+    h2 = temp2
+    for n2 in range(1, 4):
+        temp2 = np.dot(temp2, a)
+        h2 = np.vstack((h2, temp2))
+    d = np.linalg.matrix_rank(h2)
+    if d == 4:
+        print('Wo=', h2, '为列满秩')
+        print("系统是完全能观的。")
+    else:
+        print('Wo=', h2, '不为列满秩')
+        print("系统是不完全能观的。")
+        exit(1)
+
+    print('正在计算系统开环响应...')
+    x = np.array([[0.0], [0.0], [0.0], [0.0]])
+    y = x.T
+    if input('\n是否默认阶跃大小为4N：Enter/n') != 'n':
+        f = 4.0
+    else:
+        f = min(5.0, max(-5.0, float(input('请输入阶跃大小：'))))
+    dt = 0.01
+    times = 200
+    rd = np.zeros(times)
+    for i in range(1, times):
+        x = x + (np.dot(a, x) + np.dot(b, [[f]])) * dt
+        y = np.vstack((y, x.T))
+    if input('\n是否作图开环阶跃响应：Enter/n') != 'n':
+        track(y, times, rd, 1, times, f)
+    if input('\n是否动画演示：Enter/n') != 'n':
+        animated(y, times)
 
     print('\n正在计算系统开环极点...')
     r = Symbol('r', complex=True)
@@ -142,64 +183,83 @@ def calculate(a, b):
     k_4 = kr[k4].evalf()
     kf = np.array([[k_1, k_2, k_3, k_4]])
     print("状态反馈矩阵：", kf)
+
+    print('阶跃大小不变，正在计算系统闭环响应...')
+    x = np.array([[0.0], [0.0], [0.0], [0.0]])
+    z = np.zeros((4, 1))
+    y = x.T
+    times = 2000
+    rd = np.zeros(times)
+    for i2 in range(1, times):
+        x = x + ((np.dot((a - (np.dot(b, kf))), x-z)) + np.dot(b, [[f]])) * dt
+        y = np.vstack((y, x.T))
+    if input('\n是否作图闭环阶跃响应：Enter/n') != 'n':
+        track(y, times, rd, 1, times, f)
+    if input('\n是否动画演示：Enter/n') != 'n':
+        animated(y, times)
+
     return kf
 
 
 def control(k):
-    print('\n初始摆杆角度：-15度\n初始摆杆角速度：-1.5弧度/秒\n初始小车位置：2.5米\n初始小车速度：-1.5米/秒')
+    print('\n初始摆杆角度：-15度\n初始摆杆角速度：-1.5弧度/秒\n'
+          '初始小车位置：2.5米\n初始小车速度：-1.5米/秒')
     if input('是否默认初始倒立摆状态：Enter/n') != 'n':
         x1 = -15.0/180 * np.pi
         x2 = -1.5
         x3 = 2.5
         x4 = -1.5
     else:
-        x1 = min(20.0, max(-20.0, float(input('请输入摆杆角度(小于20度)：')))) / 180 * np.pi
-        x2 = 0.0
-        x3 = min(4.0, max(-4.0, float(input('请输入小车位置：'))))
-        x4 = 0.0
+        x1 = min(20.0, max(-20.0, float(
+            input('请输入初始摆杆角度(绝对值小于20度)：')))) / 180 * np.pi
+        x2 = min(2.0, max(-2.0, float(
+            input('请输入初始摆杆角速度(绝对值小于2度/秒)：')))) / 180 * np.pi
+        x3 = min(10.0, max(-10.0, float(
+            input('请输入初始小车位置(绝对值小于10米)：'))))
+        x4 = min(2.0, max(-2.0, float(
+            input('请输入初始小车速度(绝对值小于2米/秒)：'))))
     x = np.array([[x1], [x2], [x3], [x4]])
     y = x.T
     z = np.zeros((4, 1))
     dt = 0.01
-    time = 2000
-    sign = True
+    times = 2000
     rd = np.array([0])
     if input('\n是否产生随机干扰信号：Enter/n') != 'n':
-        d = 1
-        sign = False
+        for jr in range(1, times):
+            r = 0.0
+            if jr%4==0:
+                r = random.uniform(-0.6, 0.6)
+            rd = np.vstack((rd, r))
     else:
-        d = 0
-    s = np.zeros(2000)
-    step = 0
-    if input('\n是否在随机时间产生阶跃信号：Enter/n') != 'n':
-        step = random.randint(800, 1000)
-        for j in range(step, 1000):
-            s[j] = 4
-        sign = False
+        rd = np.zeros(times)
+    s = np.zeros(times)
+    step1 = 0
+    if input('\n是否默认阶跃大小为3N：Enter/n') != 'n':
+        size = 3.0
+    else:
+        size = min(5.0, max(-5.0, float(input('请输入阶跃大小：'))))
+    step0 = times
+    if input('\n是否在随机一段时间产生方波推力：Enter/n') != 'n':
+        step1 = random.randint(700, 800)
+        step0 = random.randint(1000, 1200)
+        for j in range(step1, step0):
+            s[j] = size
     print('\n正在进行状态反馈动态控制...')
-    for i in range(1, 2000):
-        if (not sign) and i%3==0:
-            r = random.uniform(-0.5, 0.5) * d
-        else:
-            r = 0
-        rd = np.vstack((rd, r))
-        x = x + ((np.dot((A - (np.dot(B, k))), (x - z))) + np.dot(B, r+s[i])) * dt
+    for i in range(1, times):
+        x = x + ((np.dot((A - (np.dot(B, k))), (x - z))) + np.dot(B, [rd[i]+s[i]])) * dt
         y = np.vstack((y, x.T))
-        if (sign) and (abs(x[0])<1e-2 and abs(x[1])<1e-2 and abs(x[2])<1e-2 and abs(x[3])<1e-2):
-            time = i
-            break
-    return y, time, rd, step
+    return y, rd, step1, step0, times, size
 
 
 print('Start')
 while(True):
     A, B = model()
     K = calculate(A, B)
-    Y, T, Rd, Step = control(K)
+    Y, Rd, Step1, Step0, T, Size = control(K)
     if input('\n是否作动画：Enter/n') != 'n':
         animated(Y, T)
     if input('\n是否作轨迹图：Enter/n') != 'n':
-        track(Y, T, Rd, Step)
+        track(Y, T, Rd, Step1, Step0, Size)
     if input('\n是否继续：Enter/n') == 'n':
         print('End')
         break
